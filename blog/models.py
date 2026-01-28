cd61b3ec41fc96c98f361bf21e1fa9a63a7e0933
makarmee <makarmee@gmail.com>
2026-01-28 15:10:16 +0430

    Admin

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.core.validators import MinLengthValidator
 # Create your models here.
 class Tag(models.Model):
   caption=models.CharField(max_length=20)
-  
+
 
 class Author(models.Model):
   first_name=models.CharField(max_length=100)
@@ -18,5 +18,5 @@ class Post(models.Model):
   date=models.DateField(auto_now=True)
   slug=models.SlugField(unique=True,db_index=True)
   content=models.TextField(validators=[MinLengthValidator(10)])
-  author=models.ForeignKey(Author,on_delete=models.SET_NULL,related_name='posts')
+  author=models.ForeignKey(Author,on_delete=models.SET_NULL,null=True,related_name='posts')
   tags=models.ManyToManyField(Tag)
