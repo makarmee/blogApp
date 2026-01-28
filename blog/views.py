bc617752b6763faa2fab0977668ac68af8efadd9
makarmee <makarmee@gmail.com>
2026-01-28 15:37:46 +0430

    fetching posts for starting page

--- blog/views.py
+++ blog/views.py
@@ -1,49 +1,9 @@
 from datetime import date
 from django.shortcuts import render
+from .models import Post
 
-allPosts=[{
-  'slug':'hike-in-the-mountian',
-  'image':'mountain.png',
-  'author':'mahdi',
-  'date':date(2025,1,5),
-  'title':'Mountain Hiking',
-  'excerpt':"There is nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoting the view.",
-  'content':"""
-  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi est iusto natus maxime corporis veritatis perferendis optio error adipisci ad laborum, quas esse, quo et? Qui consequatur consectetur quia quam.
-  
-  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi est iusto natus maxime corporis veritatis perferendis optio error adipisci ad laborum, quas esse, quo et? Qui consequatur consectetur quia quam.
-  
-  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi est iusto natus maxime corporis veritatis perferendis optio error adipisci ad laborum, quas esse, quo et? Qui consequatur consectetur quia quam.
-  """
-},{
-  'slug':'into-the-woods',
-  'image':'wood.png',
-  'author':'mahdi',
-  'date':date(2025,1,7),
-  'title':'Nature At its Best!',
-  'excerpt':"Dolor cupidatat consequat duis reprehenderit sit aute aliqua sint officia.",
-  'content':"""
-  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi est iusto natus maxime corporis veritatis perferendis optio error adipisci ad laborum, quas esse, quo et? Qui consequatur consectetur quia quam.
-  
-  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi est iusto natus maxime corporis veritatis perferendis optio error adipisci ad laborum, quas esse, quo et? Qui consequatur consectetur quia quam.
-  
-  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi est iusto natus maxime corporis veritatis perferendis optio error adipisci ad laborum, quas esse, quo et? Qui consequatur consectetur quia quam.
-  """
-},{
-  'slug':'programming-is-fun',
-  'image':'coding.png',
-  'author':'mahdi',
-  'date':date(2025,1,5),
-  'title':'Programming is Great!',
-  'excerpt':"Mollit incididunt ullamco id sint et fugiat adipisicing eu sunt proident dolore magna mollit.",
-  'content':"""
-  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi est iusto natus maxime corporis veritatis perferendis optio error adipisci ad laborum, quas esse, quo et? Qui consequatur consectetur quia quam.
-  
-  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi est iusto natus maxime corporis veritatis perferendis optio error adipisci ad laborum, quas esse, quo et? Qui consequatur consectetur quia quam.
-  
-  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi est iusto natus maxime corporis veritatis perferendis optio error adipisci ad laborum, quas esse, quo et? Qui consequatur consectetur quia quam.
-  """
-}]
+
+allPosts=[]
 
 
 def get_date(post):
@@ -51,8 +11,7 @@ def get_date(post):
 
 # Create your views here.
 def starting_page(request):
-  sortedPosts=sorted(allPosts,key=get_date)
-  latestPosts=sortedPosts[-3:]
+  latestPosts=Post.objects.all().order_by('-date')[:3]
   return render(request,'blog/index.html',{
     'posts':latestPosts
   })
